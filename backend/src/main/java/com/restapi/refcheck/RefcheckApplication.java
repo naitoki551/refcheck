29462bc4dcffd86a513e3cccbadd5d5ed48df763
naitoki551 <inside.wisteria50u@gmail.com>
2020-06-13 16:54:24 +0900

    [add] dockerの雛形

---
+++ backend/src/main/java/com/restapi/refcheck/RefcheckApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.refcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefcheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefcheckApplication.class, args);
+	}
+
+}
--- web-app/src/main/java/com/restapi/refcheck/repository/TaskDao.java
+++
@@ -1,7 +0,0 @@
-package com.restapi.refcheck.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.restapi.refcheck.entity.Task;
-
-public interface TaskDao extends JpaRepository<Task, Integer> {
-}
